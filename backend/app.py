b84e44041b2eff732437c6e69e36f437acdc03ad
Ambika <ambika5849@gmail.com>
2026-04-03 22:13:10 +0530

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Backend is running!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+echo "# Secure-File-Sharing-System-Using-AES" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/9731080/Secure-File-Sharing-System-Using-AES.git
+git push -u origin main
